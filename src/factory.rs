e167a86c0944c95bb7656e87030b79b34302b3df
Manuel Clever <manuel-clever@web.de>
2022-12-08 22:31:31 +0100

    dec_05

--- src/factory.rs
+++ src/factory.rs
@@ -2,6 +2,7 @@ mod dec01;
 mod dec02;
 mod dec03;
 mod dec04;
+mod dec05;
 
 use std::fmt::Error;
 use crate::solve::Solve;
@@ -12,6 +13,7 @@ pub fn create_calculator(day: &i8) -> Result<Box<dyn Solve>, Error> {
         2 => {Ok(Box::new(dec02::Calculator {}))},
         3 => {Ok(Box::new(dec03::Calculator {}))},
         4 => {Ok(Box::new(dec04::Calculator {}))},
+        5 => {Ok(Box::new(dec05::Calculator {}))},
         _ => {Err(Error)}
     }
 }
